8412ded8e3678f63c93ba1d47c42e425f0be8a54
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-04-28 01:12:41 -0400

    SpringMVC 04/27/2021

---
+++ 10.SpringMVC/SpringMVC/springmvc-03-annotation/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangjinhao
+  Date: 2021/4/27
+  Time: 6:34 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
--- 10.SpringMVC/SpringMVC/springmvc-04-controller/src/main/java/com/jin/controller/ControllerTest1.java
+++ 10.SpringMVC/SpringMVC/springmvc-04-controller/src/main/java/com/jin/controller/ControllerTest1.java
@@ -6,17 +6,13 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloController implements Controller {
+//只要实现了controller接口的类，说明这就是一个控制器了
+public class ControllerTest1 implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mv = new ModelAndView();
-
-        //业务代码
-        String result = "HelloSpringMVC";
-        mv.addObject("msg",result);
-        //视图跳转
+        mv.addObject("msg","ControllerTest1");
         mv.setViewName("test");
-
         return mv;
     }
 }

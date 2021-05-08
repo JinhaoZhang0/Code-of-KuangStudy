bad708a9c0dc520d2ba8373b566294112e521d92
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-05-08 18:11:33 -0400

    SpringMVC: module spring-06-ajax completed

--- 10.SpringMVC/SpringMVC/springmvc-06-ajax/src/main/java/com/jin/controller/AjaxController.java
+++ 10.SpringMVC/SpringMVC/springmvc-06-ajax/src/main/java/com/jin/controller/AjaxController.java
@@ -1,10 +1,13 @@
 package com.jin.controller;
 
+import com.jin.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AjaxController {
@@ -22,5 +25,37 @@ public class AjaxController {
         } else {
             response.getWriter().print("false");
         }
+    }
+
+    @RequestMapping("/a2")
+    public List<User> a2() {
+        List<User> list = new ArrayList<>();
+        //添加数据
+        list.add(new User("关羽",37,"男"));
+        list.add(new User("张飞",38,"男"));
+        list.add(new User("刘备",39,"男"));
+        return list; //由于@RestController注解，将list转成json格式返回
+    }
+
+    @RequestMapping("/a3")
+    public  String a3(String name, String pwd) {
+        String msg = "";
+        if (name != null) {
+            //admin 这些数据应该在数据库中查
+            if ("admin".equals(name)) {
+                msg = "ok";
+            } else {
+                msg = "用户名有误";
+            }
+        }
+        if (pwd != null) {
+            //admin 这些数据应该在数据库中查
+            if ("123456".equals(pwd)) {
+                msg = "ok";
+            } else {
+                msg = "密码有误";
+            }
+        }
+        return msg;
     }
 }

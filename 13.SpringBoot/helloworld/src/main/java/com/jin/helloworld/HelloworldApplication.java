14fc3135b0cd1b99483326aca29fd25995d5fb9c
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-05-12 22:34:18 -0400

    Create Springboot helloworld: use two methods to create helloworld file

---
+++ 13.SpringBoot/helloworld/src/main/java/com/jin/helloworld/HelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.jin.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//本身就是Spring的一个组件
+
+//程序的一个组件
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
---
+++ 13.SpringBoot/helloworld/src/test/java/com/jin/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

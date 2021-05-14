6c22617072651519402bb6c4e8d5d29cbfa25d33
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-05-13 23:18:25 -0400

    Update SpringBoot-02-config

--- 13.SpringBoot/springboot-02-config/src/main/java/com/jin/Springboot02ConfigApplication.java
+++ 13.SpringBoot/springboot-02-config/src/main/java/com/jin/Springboot02ConfigApplication.java
@@ -3,12 +3,11 @@ package com.jin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//标注这个类是一个SpringBoot应用
 @SpringBootApplication
-public class Springboot01HelloworldApplication {
+public class Springboot02ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot01HelloworldApplication.class, args);
+        SpringApplication.run(Springboot02ConfigApplication.class, args);
     }
 
 }

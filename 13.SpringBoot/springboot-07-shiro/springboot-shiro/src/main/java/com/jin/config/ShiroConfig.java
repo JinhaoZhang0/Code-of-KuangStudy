e1acdb827038d5d6ac13c45f2881d8128f90afc4
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-05-24 18:19:33 -0400

    Create springboot-07-shiro: hello-shiro and springboot-shiro

---
+++ 13.SpringBoot/springboot-07-shiro/springboot-shiro/src/main/java/com/jin/config/ShiroConfig.java
@@ -0,0 +1,35 @@
+package com.jin.config;
+
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+    //ShiroFilterFactoryBean
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager defaultWebSecurityManager){
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        //设置安全管理器
+        bean.setSecurityManager(defaultWebSecurityManager);
+        return bean;
+    }
+
+    //DefaultWebSecurityManager
+    @Bean(name="securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm") UserRealm userRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //关联UserRealm
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+
+    //创建 realm 对象，需要自定义类
+    @Bean
+    public UserRealm userRealm() {
+        return new UserRealm();
+    }
+}

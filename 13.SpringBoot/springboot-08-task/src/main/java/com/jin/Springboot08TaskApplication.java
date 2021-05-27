62c0860bce0fda73f80fcd371f08ab7bef86a248
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-05-26 21:22:27 -0400

    Update SpringBoot/spring-08-task

--- 13.SpringBoot/springboot-08-task/src/main/java/com/jin/Springboot08TaskApplication.java
+++ 13.SpringBoot/springboot-08-task/src/main/java/com/jin/Springboot08TaskApplication.java
@@ -2,7 +2,11 @@ package com.jin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync //开启异步注解功能
+@EnableScheduling //开启定时功能
 @SpringBootApplication
 public class Springboot08TaskApplication {
 

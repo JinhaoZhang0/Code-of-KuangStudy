68263c653f4774ae30f26afbfe69708525982118
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-06-06 19:37:15 -0400

    Update springcloud: fix springcloud-api pojo code; update springcloud-provider-dept-8001: RestFul style part

--- 14.SpringCloud/springcloud/springcloud-api/src/main/java/com/jin/springcloud/pojo/Dept.java
+++ 14.SpringCloud/springcloud/springcloud-api/src/main/java/com/jin/springcloud/pojo/Dept.java
@@ -7,7 +7,6 @@ import lombok.experimental.Accessors;
 import java.io.Serializable;
 
 @Data
-@NoArgsConstructor
 @Accessors(chain = true) //链式写法
 public class Dept implements Serializable { //Dept 实体类 orm 类表关系映射
     private Long deptno; //主键
@@ -15,8 +14,35 @@ public class Dept implements Serializable { //Dept 实体类 orm 类表关系映
     //这个数据是存在那个数据库的字段～ 微服务，一个服务对应一个数据库，同一个信息可能存在不同的数据库
     private String db_source;
 
+    public Dept() {
+    }
+
     public Dept(String dname) {
         this.dname = dname;
+    }
+
+    public Long getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Long deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getDb_source() {
+        return db_source;
+    }
+
+    public void setDb_source(String db_source) {
+        this.db_source = db_source;
     }
 
     /*

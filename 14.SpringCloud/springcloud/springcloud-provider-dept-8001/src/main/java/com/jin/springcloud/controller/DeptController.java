ae50ae76ed158deeb2d2757377341a7582ab79a6
Jinhao Zhang <jinhaozhang777@gmail.com>
2021-06-08 14:16:42 -0400

    Update springcloud: Register springcloud-provider-dept-8001 in eureka module

--- 14.SpringCloud/springcloud/springcloud-provider-dept-8001/src/main/java/com/jin/springcloud/controller/DeptController.java
+++ 14.SpringCloud/springcloud/springcloud-provider-dept-8001/src/main/java/com/jin/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.jin.springcloud.controller;
 import com.jin.springcloud.pojo.Dept;
 import com.jin.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+    //可以用来获取一些配置的信息，得到具体的微服务！
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept) {
@@ -29,5 +34,25 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> addDept() {
         return deptService.queryAll();
+    }
+
+    //注册进来的微服务～，获取一些消息～
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+        //获取微服务列表的清单
+        List<String> services = client.getServices();
+        System.out.println("dicovery=>services:" + services);
+
+        //得到一个具体的微服务信息，通过具体的微服务id，applicationName
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" + // 主机名称
+                            instance.getPort() + "\t" + // 端口号
+                            instance.getUri() + "\t" + // uri
+                            instance.getServiceId() // 服务id
+            );
+        }
+        return this.client;
     }
 }
